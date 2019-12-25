65ca7bd3d37a6e34e1910fdefa00e10144979f70
Jordan Bertieaux <22647816+SH4RKNANDO@users.noreply.github.com>
2019-12-25 13:04:36 +0100

    Update install.sh
    Add OS Detection

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+function check_system {
+
+  OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+  ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+  VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
+
+  if [ -z "$OS" ]; then
+      OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
+  fi
+
+  if [ -z "$VERSION" ]; then
+      VERSION=$(awk '{print $3}' /etc/*-release)
+  fi
+
+  echo $OS
+  echo $ARCH
+  echo $VERSION
+}
+
+check_system
+exit
+
 PACKAGES="python-virtualenvwrapper python-virtualenv python-numpy opencv opencv-samples"
 
 pacman -Syy
@@ -9,4 +31,3 @@ do
   echo -e "\nInstall package : $p\n"
   sudo pacman -S $p
 done
-
