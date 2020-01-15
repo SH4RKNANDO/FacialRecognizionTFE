83eac5af1dd2302390407f106d674e06fb2167e6
Jordan Bertieaux <jordan.bertieaux@std.heh.be>
2020-01-15 16:19:07 +0100

    Perform Face Recognizion

--- FacialRecognizion/main2.py
+++ FacialRecognizion/main2.py
@@ -1,21 +1,24 @@
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# ===========================================================================
+#           Definition of Import
+# ===========================================================================
 # *-----------------------*
 # | Import faces Detector |
 # *-----------------------*
+# from FaceDetector.FaceDetector import FaceDetector
 from FaceDetector.FaceDetectorDNN import FaceDetectorDNN
 from FaceDetector.FaceDetectorHaar import FaceDetectorHaar
 from FaceDetector.FaceDetectorHog import FaceDetectorHoG
 from FaceDetector.FaceDetectorMMOD import FaceDetectorMMOD
 from FaceDetector.FaceDetectorTINY import FaceDetectorTINY
 from FaceDetector import tiny_face_model
-# from FaceDetector.FaceDetector import FaceDetector
 
 # *------------------------*
 # | Import Object Detector |
 # *------------------------*
 from ObjectDetector.ObjectDetector import ObjectDetector
-from ObjectDetector.ObjectDetector_old import ObjectDetector
 
 # *------------------------*
 # | Import Python Library  |
@@ -23,7 +26,6 @@ from ObjectDetector.ObjectDetector_old import ObjectDetector
 from configparser import ConfigParser
 from imutils import paths
 from os import path
-
 import argparse
 import re
 import cv2
@@ -32,6 +34,17 @@ import numpy as np
 import os
 import dlib
 
+# ===========================================================================
+#           Infos developer
+# ===========================================================================
+__author__ = "Jordan BERTIEAUX"
+__copyright__ = "Copyright 2020, Facial Recognition"
+__credits__ = ["Jordan BERTIEAUX"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Jordan BERTIEAUX"
+__email__ = "jordan.bertieaux@std.heh.be"
+__status__ = "Production"
 
 # =========================================== < HELPERS FUNCTION > ====================================================
 
@@ -44,6 +57,21 @@ def _convert_boolean(string):
         return True
     else:
         return False
+
+
+def _top():
+    os.system("clear")
+    print("\n*-----------------------------------------------------*")
+    print("| __author__ = Jordan BERTIEAUX                       |")
+    print("| __copyright__ = Copyright 2020, Facial Recognition  |")
+    print("| __credits__ = [Jordan BERTIEAUX]                    |")
+    print("| __license__ = GPL                                   |")
+    print("| __version__ = 1.0                                   |")
+    print("| __maintainer__ = Jordan BERTIEAUX                   |")
+    print("| __email__ = jordan.bertieaux@std.heh.be             |")
+    print("| __status__ = Production                             |")
+    print("*-----------------------------------------------------*\n")
+
 
 # =========================================== < DETECTOR FUNCTION > ===================================================
 
@@ -73,18 +101,14 @@ def create_object_detector():
     elif not path.isfile(config['yolo_config_path']):
         raise Exception("Error : config_path no such file or directory : {0}".format(config['yolo_config_path']))
 
-    obj = ObjectDetector(float(config['confidence']),
-                            float(config['threshold']),
-                            [LABELS, COLORS, NET],
-                            _convert_boolean(config['yolo_show_percent']),
-                            _convert_boolean(config['yolo_override_ZM']),
-                            config['detect_pattern'])
+    obj = ObjectDetector(float(config['confidence']), float(config['threshold']), [LABELS, COLORS, NET],
+                         _convert_boolean(config['yolo_show_percent']), _convert_boolean(config['yolo_override_ZM']),
+                         config['detect_pattern'])
     # clean the RAM
     del config
     del LABELS
     del COLORS
     del NET
-
     return obj
 
 
@@ -97,8 +121,24 @@ def create_face_detector():
     config = config['General']
     face_detector = None
 
+    # ELIF FACEDETECTOR == Tiny
+    if config['face_detector_process'] == "Tiny":
+        config = ConfigParser()
+        config.read('Data/Config/detector.ini')
+        config = config['FaceDetectorTiny']
+
+        if path.isfile(config['Tiny_Face_detection_model']):
+            face_detector = FaceDetectorTINY(MAX_INPUT_DIM=config['MAX_INPUT_DIM'],
+                                             prob_thresh=float(config['prob_thresh']),
+                                             nms_thres=float(config['nms_tresh']),
+                                             lw=int(config['lw']),
+                                             model=tiny_face_model.Model(config['Tiny_Face_detection_model']))
+        else:
+            raise Exception(
+                "[ERROR] MMOD Model no such file or directory : ".format(config['Tiny_Face_detection_model']))
+
     # IF FACEDETECTOR == DNN
-    if config['face_detector_process'] == "DNN":
+    elif config['face_detector_process'] == "DNN":
         config = ConfigParser()
         config.read('Data/Config/detector.ini')
         config = config['FaceDetectorDNN']
@@ -143,22 +183,6 @@ def create_face_detector():
     elif config['face_detector_process'] == "HoG":
         face_detector = FaceDetectorHoG()
 
-    # ELIF FACEDETECTOR == Tiny
-    elif config['face_detector_process'] == "Tiny":
-        config = ConfigParser()
-        config.read('Data/Config/detector.ini')
-        config = config['FaceDetectorTiny']
-
-        if path.isfile(config['Tiny_Face_detection_model']):
-            face_detector = FaceDetectorTINY(MAX_INPUT_DIM=config['MAX_INPUT_DIM'],
-                                             prob_thresh=float(config['prob_thresh']),
-                                             nms_thres=float(config['nms_tresh']),
-                                             lw=int(config['lw']),
-                                             model=tiny_face_model.Model(config['Tiny_Face_detection_model']))
-        else:
-            raise Exception(
-                "[ERROR] MMOD Model no such file or directory : ".format(config['Tiny_Face_detection_model']))
-
     # ELIF FACEDETECTOR == ERROR
     else:
         raise Exception("ERROR No FaceDetector Selected into detector.ini")
@@ -170,6 +194,7 @@ def create_face_detector():
 # ============================================= < MAIN FUNCTION > =====================================================
 
 if __name__ == "__main__":
+    _top()
     ap = argparse.ArgumentParser()
     ap.add_argument('-p', '--eventpath', help='Path of Events Directory', default='IMAGE_TO_DETECT')
     args, u = ap.parse_known_args()
@@ -178,6 +203,7 @@ if __name__ == "__main__":
     # *=============================*
     # |  Read the ini config file   |
     # *=============================*
+    print("[INFOS ] Reading detector config ...")
     config = ConfigParser()
     config.read('Data/Config/detector.ini')
     config = config['General']
@@ -186,13 +212,17 @@ if __name__ == "__main__":
         images = list(paths.list_images(args['eventpath']))
         cpt = 0
 
-        # *=============================*
-        # | Create Face/object detector |
-        # *=============================*
-        object_detector = create_object_detector()
-        face_detector = FaceDetector()
+        if len(images) < 1:
+            print("No Images Detected")
+        else:
+            # *=============================*
+            # | Create Face/object detector |
+            # *=============================*
+            print("[INFOS ] Loading object detector ...")
+            object_detector = create_object_detector()
+            print("[INFOS ] Loading face detector ...")
+            face_detector = create_face_detector()
 
-        if len(images) > 1:
             for image in images:
                 start = time.time()
                 cpt += 1
@@ -201,31 +231,23 @@ if __name__ == "__main__":
                 object_detector.image_path = image
                 yolo_result = object_detector.run()
 
-                if yolo_result == 'person' and _convert_boolean(config['use_facial_recognizion']):
-                    print("Recognizing Process...")
-                    frame = face_detector.detectFaceDlibHog(cv2.imread(image))
-                    frame2 = face_detector.detectFaceOpenCVHaar(cv2.imread(image))
-                    frame3 = face_detector.detectFaceOpenCVDnn(cv2.imread(image))
-                    if frame[1] > 0 or frame2[1] > 0 or frame3[1] > 0:
-                        print(image)
-                        cv2.imshow("DEBUG", frame[0])
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
-                        cv2.imshow("DEBUG", frame2[0])
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
-                        cv2.imshow("DEBUG", frame3[0])
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
-                    else:
-                        print('No Faces Detected')
-                elif yolo_result == 'car' and _convert_boolean(config['use_alpr']):
-                    print("CAR AND ALPR")
-                    # TODO ALPR RECOGNIZING
-                else:
-                    os.system("rm -rfv " + image)
-                    print("Nothing Detected")
-
-                print("Processing Time {0} Seconds".format(round(time.time()-start, 2)))
-        else:
-            print("No Images Detected")
+    #             if yolo_result == 'person' and _convert_boolean(config['use_facial_recognizion']):
+    #                 print("Recognizing Process...")
+    #                 frame = face_detector.detectFace(cv2.imread(image))
+    #                 if frame[1] > 0 or frame2[1] > 0 or frame3[1] > 0:
+    #                     print(image)
+    #                     cv2.imshow("DEBUG", frame[0])
+    #                     cv2.waitKey(0)
+    #                     cv2.destroyAllWindows()
+    #                 else:
+    #                     print('No Faces Detected')
+    #             elif yolo_result == 'car' and _convert_boolean(config['use_alpr']):
+    #                 print("CAR AND ALPR")
+    #                 # TODO ALPR RECOGNIZING
+    #             else:
+    #                 os.system("rm -rfv " + image)
+    #                 print("Nothing Detected")
+    #
+    #             print("Processing Time {0} Seconds".format(round(time.time()-start, 2)))
+    else:
+        print("Folder Image Not Found : " + str(args['eventpath']))
